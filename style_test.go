c9df7221d3800c71c927c85d95499b3ed05b4b75
Erik Geiser <erik.geiser@posteo.net>
2021-08-24 14:49:23 +0200

    Environment independent faint test

--- style_test.go
+++ style_test.go
@@ -30,7 +30,23 @@ func TestStyleWidth(t *testing.T) {
 func TestForceFaint(t *testing.T) {
 	s := String("Hello World").Foreground(TrueColor.Color("#40ff00")).Background(TrueColor.Color("#605e10")).ForceFaint()
 
-	exp := "\x1b[38;2;80;175;8;48;2;96;94;16mHello World\x1b[0m"
+	var exp string
+
+	p := ColorProfile()
+
+	switch p {
+	case Ascii:
+		exp = "\x1b[48;2;96;94;16mHello World\x1b[0m"
+	case ANSI:
+		exp = "\x1b[32;48;2;96;94;16mHello World\x1b[0m"
+	case ANSI256:
+		exp = "\x1b[38;5;70;48;2;96;94;16mHello World\x1b[0m"
+	case TrueColor:
+		exp = "\x1b[38;2;80;175;8;48;2;96;94;16mHello World\x1b[0m"
+	default:
+		t.Fatalf("missing test value for color profile %d", p)
+	}
+
 	if s.String() != exp {
 		t.Errorf("Expected %s (%q), got %s (%q)", exp, exp, s.String(), s.String())
 	}
