4e0b03c6a13974aece410d8099955c0bcbddc5ac
Christian Muehlhaeuser <muesli@gmail.com>
2022-02-02 13:38:38 +0100

    Add Windows color support detection

--- termenv_windows.go
+++ termenv_windows.go
@@ -4,10 +4,38 @@
 package termenv
 
 import (
+	"os"
+	"strconv"
+
 	"golang.org/x/sys/windows"
 )
 
 func colorProfile() Profile {
+	if os.Getenv("ConEmuANSI") == "ON" {
+		return TrueColor
+	}
+
+	winVersion, _, buildNumber := windows.RtlGetNtVersionNumbers()
+	if buildNumber < 10586 || winVersion < 10 {
+		// No ANSI support before Windows 10 build 10586.
+		if os.Getenv("ANSICON") != "" {
+			conVersion := os.Getenv("ANSICON_VER")
+			cv, err := strconv.ParseInt(conVersion, 10, 64)
+			if err != nil || cv < 181 {
+				// No 8 bit color support before v1.81 release.
+				return ANSI
+			}
+
+			return ANSI256
+		}
+
+		return Ascii
+	}
+	if buildNumber < 14931 {
+		// No true color support before build 14931.
+		return ANSI256
+	}
+
 	return TrueColor
 }
 
