muesli/termenv

commit 7d897462ce604c1c0e98db6e4e6b3191a11b21dc

fix: compiling js/wasm/plan9/aix failed missing method ColorProfile and EnableVirtualTerminalProcessing

termenv_other.go
@@ -3,6 +3,8 @@
 
 package termenv
 
+import "io"
+
 func colorProfile() Profile {
 	return ANSI256
 }
@@ -15,4 +17,18 @@ func (o Output) foregroundColor() Color {
 func (o Output) backgroundColor() Color {
 	// default black
 	return ANSIColor(0)
+}
+
+// ColorProfile returns the supported color profile:
+// Default ANSI
+func (o *Output) ColorProfile() Profile {
+	return ANSI
+}
+
+// EnableVirtualTerminalProcessing enables virtual terminal processing on
+// Windows for w and returns a function that restores w to its previous state.
+// On non-Windows platforms, or if w does not refer to a terminal, then it
+// returns a non-nil no-op function and no error.
+func EnableVirtualTerminalProcessing(w io.Writer) (func() error, error) {
+	return func() error { return nil }, nil
 }

commit 38481645b0c3e388f4ca8072968b1d2abb0be6d2

fix(termenv): remove unused method
Fixes: 7d897462ce60 ("fix: compiling js/wasm/plan9/aix failed missing method ColorProfile and EnableVirtualTerminalProcessing")

termenv_other.go
@@ -5,7 +5,9 @@ package termenv
 
 import "io"
 
-func colorProfile() Profile {
+// ColorProfile returns the supported color profile:
+// ANSI256
+func (o Output) ColorProfile() Profile {
 	return ANSI256
 }
 
@@ -17,12 +19,6 @@ func (o Output) foregroundColor() Color {
 func (o Output) backgroundColor() Color {
 	// default black
 	return ANSIColor(0)
-}
-
-// ColorProfile returns the supported color profile:
-// Default ANSI
-func (o *Output) ColorProfile() Profile {
-	return ANSI
 }
 
 // EnableVirtualTerminalProcessing enables virtual terminal processing on